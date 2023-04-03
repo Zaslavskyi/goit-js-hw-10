110874b55df3a7c72a33fae5c6f6f292590662a4
Zaslavskyi <bbaly89@gmail.com>
2023-04-04 02:07:42 +0300

    ///

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,11 +1,11 @@
-export default function fetchCountryName(name) {
-    const responseFilter = `?fields=name,capital,population,flags,languages`;
-
+export default function fetchCountries(name) {
+    const responseFilter = '?fields=name,capital,population,flags,languages';
+    
     return fetch(`https://restcountries.com/v3.1/name/${name}${responseFilter}`)
-    .then(response => {
+        .then(response => {
         if (!response.ok) {
             throw new Error(response.status);
-        };
+        }
         return response.json();
-    });
-};
+        })
+    }
--- src/index.js
+++ src/index.js
@@ -1,81 +1,95 @@
 import './css/styles.css';
-import debounce from 'lodash.debounce';
 import Notiflix from 'notiflix';
+import debounce from 'lodash.debounce';
 import fetchCountries from './fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
 
-const inputElement = document.querySelector('[id = "search-box"]');
-const listElement = document.querySelector('.country-list');
-const divElement = document.querySelector('.country-info');
-
+const formInputEl = document.querySelector('[id="search-box"]');
+const listEl = document.querySelector('.country-list');
+const counrtyInfoEl = document.querySelector('.country-info');
 let markup = '';
 
-
-inputElement.addEventListener('input', debounce(onInputSearch, DEBOUNCE_DELAY));
+formInputEl.addEventListener('input', debounce(onInputSearch, DEBOUNCE_DELAY));
 
 function onInputSearch(e) {
     e.preventDefault();
 
-    const formValue = inputElement.value.trim();
+    const formValue = formInputEl.value.trim();
     clearHTML();
 
     if (formValue === '') {
-        return;
+    return;
     }
 
-    fetchCountries(formValue).then(onUpdateUi).catch(showError);
+    fetchCountries(formValue).then(onUpdateUI).catch(showError);
 }
 
-function onUpdateUi(countries) {
+function onUpdateUI(countries) {
     if (!countries) {
-        return;
+    return;
     }
 
     if (countries.length > 10) {
-        Notiflix.Notify.info('"Too many matches found. Please enter a more specific name." ');
-        return;
-    };
+    Notiflix.Notify.info(
+        '"Too many matches found. Please enter a more specific name."'
+    );
+    return;
+    }
 
-    if (countries.length > 1) {
-        return makeCountriesList(countries);
-    };
+    if (countries.length > 1 && countries.length < 10) {
+    return makeCountriesListUI(countries);
+    }
 
     if (countries.length === 1) {
-        return makeCountriesInfo(countries);
-    };
-};
+    return makeCountryInfoUI(countries);
+    }
+}
 
-function makeCountriesList(countries) {
+function makeCountriesListUI(countries) {
     markup = countries
     .map(country => {
-        return `<li class="item"><img class="flag" src=${country.flag.svg} width="30"/><p class="country-name--small">${country.name.common}</p></li>`;
+        return `
+        <li class='item'>
+            <img class="flag" src=${country.flags.svg} width="30"/>
+            <p class="country-name--small">${country.name.common}</p>
+        </li>`;
     })
     .join('');
 
-    return listElement.insertAdjacentHTML('afterbegin', markup);
+    return listEl.insertAdjacentHTML('afterbegin', markup);
 }
 
-function makeCountriesInfo(country) {
+function makeCountryInfoUI(country) {
     markup = country
     .map(country => {
         return `
-        <div class="country"> <span><img class"flag" src=${country.flags.svg} width="30"/></span>
-        <p class="country-name--big">${country.name.common}</p>
+            <span>
+                <img class="flag" src=${country.flags.svg} width="30"/>
+            </span>
+            <p class="country-name--big">${country.name.common}</p>
         <ul class="country-list">
-        <li class="country-list__item"><p class="country-list__text"> Capital:${country.capital}</p></li>
-        <li class="country-list__item"><p calss="country-list__text">Population:${country.population}</p></li>
-        <li class="country-list__item"><p class="country-list__text">Languages:${Object.values(country.languages).join(',')}</p></li></ul></div>`;
+            <li class="country-list__item">
+                <p class="country-list__text">Capital: ${country.capital}</p>
+            </li>
+            <li class="country-list__item">
+                <p class="country-list__text">Population: ${country.population}</p>
+            </li>
+            <li class="country-list__item">
+                <p class="country-list__text">Languages: ${Object.values(country.languages).join(', ')}</p>
+            </li>
+        </ul>`;
     })
     .join('');
-    return divElement.insertAdjacentHTML('afterbegin', markup);
-};
+
+    return counrtyInfoEl.insertAdjacentHTML('afterbegin', markup);
+}
 
 function clearHTML() {
-    divElement.innerHTML = '';
-    listElement.innerHTML = '';
-};
+    counrtyInfoEl.innerHTML = '';
+    listEl.innerHTML = '';
+}
 
 function showError() {
-    return Notiflix.Notify.failure('Oops, there is no country with that name...');
+    return Notiflix.Notify.failure('Oops, there is no country with that name');
 }
